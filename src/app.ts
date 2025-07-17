047405bf0d4339940ffe53bdff25c028316a2191
Shachiru <shachirurashmika35@gmail.com>
2025-07-17 15:28:56 +0530

    reinstalled dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import express, {Express} from "express";
+import cors from "cors";
 
 const app: Express = express();
 
 const allowedOrigins = [
     "http://localhost:5173"
 ];
+
+const corsOptions = {
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+}
+
+app.use(cors(corsOptions));
 
 export default app;

6ad6f8a26d1f6667da127589700a51649dabe5cc
Shachiru <shachirurashmika35@gmail.com>
2025-07-28 15:59:58 +0530

    align lines

--- src/services/borrowingList.service.ts
+++ src/services/borrowingList.service.ts
@@ -1,26 +1,26 @@
 import mongoose from "mongoose";
 import BorrowingList from "../model/borrowingList.model";
 import Book from "../model/book.model";
-import { BorrowingListDTO } from "../dto/borrowingList.dto";
+import {BorrowingListDTO} from "../dto/borrowingList.dto";
 
 export const getBorrowingListByUserId = async (userId: string): Promise<BorrowingListDTO | null> => {
     if (!mongoose.Types.ObjectId.isValid(userId)) {
         return null;
     }
-    return BorrowingList.findOne({ userId }).populate('books');
+    return BorrowingList.findOne({userId}).populate('books');
 };
 
 export const addBookToBorrowingList = async (userId: string, isbn: string): Promise<BorrowingListDTO | null> => {
     if (!mongoose.Types.ObjectId.isValid(userId)) {
         return null;
     }
-    const book = await Book.findOne({ isbn });
+    const book = await Book.findOne({isbn});
     if (!book) {
         return null;
     }
-    let list = await BorrowingList.findOne({ userId });
+    let list = await BorrowingList.findOne({userId});
     if (!list) {
-        list = new BorrowingList({ userId, books: [book._id] });
+        list = new BorrowingList({userId, books: [book._id]});
     } else {
         if (!list.books.some(id => id.toString() === book._id.toString())) {
             list.books.push(book._id);
@@ -34,11 +34,11 @@ export const removeBookFromBorrowingList = async (userId: string, isbn: string):
     if (!mongoose.Types.ObjectId.isValid(userId)) {
         return null;
     }
-    const book = await Book.findOne({ isbn });
+    const book = await Book.findOne({isbn});
     if (!book) {
         return null;
     }
-    const list = await BorrowingList.findOne({ userId });
+    const list = await BorrowingList.findOne({userId});
     if (list) {
         list.books = list.books.filter(id => id.toString() !== book._id.toString());
         list.updatedAt = new Date();
@@ -51,7 +51,7 @@ export const clearBorrowingList = async (userId: string): Promise<boolean> => {
     if (!mongoose.Types.ObjectId.isValid(userId)) {
         return false;
     }
-    const list = await BorrowingList.findOne({ userId });
+    const list = await BorrowingList.findOne({userId});
     if (list) {
         list.books = [];
         list.updatedAt = new Date();
